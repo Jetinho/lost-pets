67426d619998a350a1023d8868cc78a7ae760d2d
Julien Jet <jet.julien@gmail.com>
2023-02-21 20:37:29 +0100

    use actual date

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 10.times do
-  Pet.create(name: Faker::FunnyName.name, species: Pet::SPECIES.sample, address: Faker::Address.full_address, date: (1..365).to_a.sample)
+  Pet.create(name: Faker::FunnyName.name, species: Pet::SPECIES.sample, address: Faker::Address.full_address, date: (1..365).to_a.sample.days.ago)
 end
