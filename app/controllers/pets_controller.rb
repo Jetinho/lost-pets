0f6448649a0b7fd16b9d274bbe71824646851f05
Julien Jet <jet.julien@gmail.com>
2023-02-21 20:51:47 +0100

    add remaining actions, basic navigation, lost_on field in form and edit form

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,31 +5,39 @@ class PetsController < ApplicationController
     @pet = Pet.new
   end
 
-  # def create
-
-  # end
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.save
+    redirect_to pet_path(@pet)
+  end
 
   def index
     @pets = Pet.all
   end
 
   def show
-
   end
 
-  # def edit
+  def edit
+  end
 
-  # end
+  def update
+    @pet.update(pet_params)
+    redirect_to pet_path(@pet)
+  end
 
-  # def update
+  def destroy
+    @pet.destroy
+    redirect_to pets_path, status: :see_other
+  end
 
-  # end
-
-  # def destroy
-
-  # end
+  private
 
   def set_pet
     @pet = Pet.find(params[:id])
+  end
+
+  def pet_params
+    params.require(:pet).permit(:name, :address, :lost_on, :species)
   end
 end
